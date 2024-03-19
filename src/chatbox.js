064b36cd56250345017d2aed81be225f7f07ee57
VeRonyKaa <49157472+VeRonyKaa@users.noreply.github.com>
2024-03-19 20:32:37 +0100

    Major UPDATE!!!!!!!  Hamzi dont fuck me up I swear
    Added chart gallery + added function that adds chart to gallery when generated
    Added the menu, all the y and x axis options

--- src/chatbox.js
+++ src/chatbox.js
@@ -1,6 +1,10 @@
 // chatbox.js
+const xy = require('../data/hobbit.json')
+let yAxis = 'Age'
+let xAxis = 'Gender';
+let plotType = 'line'
 
-function setupEventListeners() {
+async function setupEventListeners() {
   const messageInput = document.getElementById("input");
   messageInput.addEventListener("keyup", (event) => {
     if (event.key === "Enter") {
@@ -11,7 +15,52 @@ function setupEventListeners() {
   sendButton.onclick = sendMessage;
   const menuButton = document.getElementById("pop-up-button");
   menuButton.onclick = openMenu;
+  const thumbnails = document.getElementsByClassName("gallery-image");
+  for (let i = 0; i < thumbnails.length; i++) {
+    thumbnails[i].addEventListener("click", () => {
+      showImage(thumbnails[i]);
+    });
+  }
+  const yAxisSelect = document.getElementById("y-axis-select");
+  yAxisSelect.addEventListener("change", (event) => {
+    yAxis = event.target.value
+    // TODO: send to RASA
+  });
+  const xAxisSelect = document.getElementById("x-axis-select");
+  xAxisSelect.addEventListener("change", (event) => {
+    xAxis = event.target.value
+    // TODO: send to RASA
+  });
+  const plotTypeSelect = document.getElementById("plot-type-select");
+  plotTypeSelect.addEventListener("change", (event) => {
+    plotType = event.target.value
+    // TODO: send to RASA
+  });
 };
+
+export function showImage(thumbnail) {
+  //print out 'this works' to the console
+  console.log(thumbnail);
+  // create overlay div with thumbnail
+  const overlay = document.getElementById("overlay");
+  overlay.src = thumbnail.src;
+  overlay.classList.remove("hidden");
+
+  const chart = document.getElementById("viz");
+  chart.classList.add("hidden");
+
+  const selected = document.getElementsByClassName("thumbnail-selected");
+  console.log(selected);
+
+  if (selected.length != 0) {
+    console.log("removing class" + selected);
+    selected[0].classList.remove("thumbnail-selected");
+    thumbnail.classList.add("thumbnail-selected");
+  } else {
+    thumbnail.classList.add("thumbnail-selected");
+  }
+
+}
 
 function openMenu() {
   const menu = document.getElementById("pop-up");
@@ -24,6 +73,37 @@ function openMenu() {
     menuButton.classList.add("rotate");
   }
 }
+
+document.addEventListener("DOMContentLoaded", async function () {
+  // Your code here
+  // create an array of all images
+  const chartLibrary = [
+    "./public/chart1.png",
+    "./public/chart2.png",
+    "./public/chart1.png",
+    "./public/chart2.png",
+    "./public/chart1.png",
+    "./public/chart2.png",
+  ];
+
+  const galleryContainer = document.getElementById("gallery-container");
+
+  //loop through the image array
+  for (let i = 0; i < chartLibrary.length; i++) {
+    //create a new image element
+    let img = new Image();
+    img = document.createElement("img");
+    img.classList.add("gallery-image");
+    //set the source of the image element
+    img.src = chartLibrary[i];
+    galleryContainer.appendChild(img);
+  }
+
+  await setupEventListeners();
+
+});
+
+
 
 async function sendMessage() {
   const messageInput = document.getElementById("input");
@@ -55,72 +135,98 @@ async function sendMessage() {
     messageInput.value = "";
 
     // Bot message
-    //const botMessage = document.createElement("p");
-    //botMessage.classList.add("received-message");
-    //botMessage.textContent = "I am a bot";
+    const botMessage = document.createElement("p");
+    botMessage.classList.add("received-message");
+    botMessage.textContent = "I am a bot";
 
-    //const botMessengerID = document.createElement("p");
-    //botMessengerID.classList.add("chatbot-id");
-    //botMessengerID.textContent = "Chatbot:";
+    const botMessengerID = document.createElement("p");
+    botMessengerID.classList.add("chatbot-id");
+    botMessengerID.textContent = "Chatbot:";
 
-    //chatbotMessageContainer.appendChild(botMessengerID);
-    //chatbotMessageContainer.appendChild(botMessage);
+    chatbotMessageContainer.appendChild(botMessengerID);
+    chatbotMessageContainer.appendChild(botMessage);
 
-    //chatContainer.appendChild(chatbotMessageContainer);
-    //return
-
-
-    const url = 'http://localhost:5005/webhooks/rest/webhook';//'https://dashboards.create.aau.dk/webhooks/rest/webhook';
-    //const url = 'https://dashboards.create.aau.dk/webhooks/rest/webhook';
-    const data = {
-      message: message
-    };
-
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-      });
-
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-
-      const responseData = await response.json();
-      const responseDataArray = responseData.messages || []
-      console.log(response)
-      console.log(responseData)
-
-      responseData.forEach(message => {
-        console.log(message.text);
-        const botMessage = document.createElement("div");
-        botMessage.classList.add("received-message");
-        botMessage.textContent = message.text;
-        chatContainer.appendChild(botMessage);
-      })
-    } catch (error) {
-      console.error('Error:', error);
-      // Handle the error as needed, e.g., show an error message to the user
-    }
-    import("./viz").then(function (viz) {
-      viz.createLineChart()
-    });
+    chatContainer.appendChild(chatbotMessageContainer);
+    return
   }
 }
 
-setupEventListeners();
+//     const url = 'http://localhost:5005/webhooks/rest/webhook';//'https://dashboards.create.aau.dk/webhooks/rest/webhook';
+//     //const url = 'https://dashboards.create.aau.dk/webhooks/rest/webhook';
+//     const data = {
+//       message: message
+//     };
+
+//     try {
+//       const response = await fetch(url, {
+//         method: 'POST',
+//         headers: {
+//           'Content-Type': 'application/json'
+//         },
+//         body: JSON.stringify(data)
+//       });
+
+//       if (!response.ok) {
+//         throw new Error(`HTTP error! Status: ${response.status}`);
+//       }
+
+//       const responseData = await response.json();
+//       const responseDataArray = responseData.messages || []
+//       console.log(response)
+//       console.log(responseData)
+
+//       responseData.forEach(message => {
+//         console.log(message.text);
+//         const botMessage = document.createElement("div");
+//         botMessage.classList.add("received-message");
+//         botMessage.textContent = message.text;
+//         chatContainer.appendChild(botMessage);
+//       })
+//     } catch (error) {
+//       console.error('Error:', error);
+//       // Handle the error as needed, e.g., show an error message to the user
+//     }
+//     import("./viz").then(function (viz) {
+//       viz.createLineChart()
+//     });
+
+//     //hide the img #overlay and remove class hidden from #viz
+//     const overlay = document.getElementById("overlay");
+//     overlay.classList.add("hidden");
+//     const chart = document.getElementById("viz");
+//     chart.classList.remove("hidden");
+//   }
+// }
+
+//setupEventListeners();
 
 
-async function getURL() {
-  const url = 'https://dashboards.create.aau.dk/webhooks/rest/webhook/status';
 
-  const response = await fetch(url);
-  if (response.status === 200) {
-    return "http://localhost:5005"
-  } else {
-    return "https://dashboards.create.aau.dk:5005"
-  }
+// async function getURL() {
+//   const url = 'https://dashboards.create.aau.dk/webhooks/rest/webhook/status';
+
+//   const response = await fetch(url);
+//   if (response.status === 200) {
+//     return "http://localhost:5005"
+//   } else {
+//     return "https://dashboards.create.aau.dk:5005"
+//   }
+// }
+
+
+const generateXAndYAxisDropdowns = () => {
+  const xDropdown = document.getElementById("x-axis-select");
+  const yDropdown = document.getElementById("y-axis-select");
+  Object.keys(xy).forEach((key) => {
+    const xoption = document.createElement("option");
+    xoption.value = key;
+    xoption.text = key;
+    const yoption = document.createElement("option");
+    yoption.value = key;
+    yoption.text = key;
+    xDropdown.appendChild(xoption);
+    yDropdown.appendChild(yoption);
+  });
+
 }
+generateXAndYAxisDropdowns()
